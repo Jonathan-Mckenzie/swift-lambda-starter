0ee69d617333817a8677a5b971da5e40ccaedd00
Jonathan McKenzie <jonathan.mckenzie@zwift.com>
2020-09-26 11:27:57 -0700

    add post test case, handle setup/teardown for multiple tests

--- Tests/swift-lambda-starter-tests/swift-lambda-starter-tests.swift
+++ Tests/swift-lambda-starter-tests/swift-lambda-starter-tests.swift
@@ -8,19 +8,23 @@ final class SwiftLambdaStarterTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        process = Process()
         process.executableURL = productsDirectory.appendingPathComponent("swift-lambda-starter")
         try? process.run()
+        sleep(1) // wait for server to come online
     }
     
     override func tearDown() {
         super.tearDown()
         process.terminate()
+        process.waitUntilExit()
     }
+
     
     func createJson(_ name: String? = nil) -> [String: Any] {
         
         let method: String = (name != nil) ? "POST" : "GET"
-        let bodyStr: String = (name != nil) ? "{\"name\":\"\(String(describing: name))\"}" : ""
+        let bodyStr: String = (name != nil) ? "{\"name\":\"\(name ?? "")\"}" : ""
         
         return [
             "routeKey": "\(method) /test",
@@ -94,6 +98,46 @@ final class SwiftLambdaStarterTests: XCTestCase {
         XCTAssertEqual(responseJson["statusCode"] as! Int, 200)
         XCTAssertEqual(responseJson["body"] as! String, "{\"message\":\"hello friend.\"}")
     }
+    
+    func testPostRequest() throws {
+        let expectation = self.expectation(description: "Http Requests")
+        let jsonData = try? JSONSerialization.data(withJSONObject: self.createJson("Elliot"))
+
+        let url = URL(string: "http://localhost:7000/invoke")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
+        
+        var responseJson: [String: Any] = [:]
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            
+            print("errors: \(String(describing: error))")
+            print("response: \(String(describing: response))")
+            
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                print("data: \(responseJSON)")
+                responseJson = responseJSON
+            }
+            
+            expectation.fulfill()
+        }
+
+        // fire the http request
+        task.resume()
+        
+        // wait for the request call back to execute
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertEqual(responseJson["statusCode"] as! Int, 200)
+        XCTAssertEqual(responseJson["body"] as! String, "{\"message\":\"Elliot, hello friend.\"}")
+    }
+    
 
     /// Returns path to the built products directory.
     var productsDirectory: URL {
@@ -109,5 +153,6 @@ final class SwiftLambdaStarterTests: XCTestCase {
 
     static var allTests = [
         ("testGetRequest", testGetRequest),
+        ("testPostRequest", testPostRequest)
     ]
 }
